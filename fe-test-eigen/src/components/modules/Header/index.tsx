f4a31c5b7ddb6dfaa02a0476dc6cc4842ab74a72
najimRizky <najimajim1@gmail.com>
2023-09-26 12:09:14 +0700

    feat(header): adjust style

--- fe-test-eigen/src/components/modules/Header/index.tsx
+++ fe-test-eigen/src/components/modules/Header/index.tsx
@@ -6,26 +6,24 @@ import { pageRoutes } from "../../../config/pageRoutes"
 const Header = () => {
   const { pathname } = useLocation()
   return (
-    <Layout className="navbar" >
-      <Layout.Header>
-        <nav>
-          <NavLink to="/" className="brand">
-            Eigen News
-          </NavLink>
-          <Menu
-            mode="horizontal"
-            defaultSelectedKeys={[pathname]}
-            className="menu"
-            items={pageRoutes.map((route) => (
-              {
-                key: route.path,
-                label: <NavLink to={route.path}>{route.name}</NavLink>
-              }
-            ))}
-          />
-        </nav>
-      </Layout.Header>
-    </Layout>
+    <Layout.Header className="navbar">
+      <nav className="container">
+        <NavLink to="/" className="brand">
+          Eigen News
+        </NavLink>
+        <Menu
+          mode="horizontal"
+          defaultSelectedKeys={[pathname]}
+          className="menu"
+          items={pageRoutes.map((route) => (
+            {
+              key: route.path,
+              label: <NavLink to={route.path}>{route.name}</NavLink>
+            }
+          ))}
+        />
+      </nav>
+    </Layout.Header>
   )
 }
 
