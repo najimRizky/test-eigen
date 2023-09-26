43ebda821f800d4cb88430ff63a62110649d78d4
najimRizky <najimajim1@gmail.com>
2023-09-26 19:25:18 +0700

    feat(header): responsive header

--- fe-test-eigen/src/components/modules/Header/index.tsx
+++ fe-test-eigen/src/components/modules/Header/index.tsx
@@ -1,27 +1,62 @@
-import { Layout, Menu } from "antd"
+import { Layout, Menu, Drawer, Button } from "antd"
 import "./style.css"
 import { NavLink, useLocation } from "react-router-dom"
-import { pageRoutes } from "../../../config/pageRoutes"
+import { pageRoutes } from "../../../config/pageRoutes" 
+import { useState } from "react"
+import { MenuOutlined } from "@ant-design/icons";
 
 const Header = () => {
   const { pathname } = useLocation()
+  const [open, setOpen] = useState(false)
+
   return (
     <Layout.Header className="navbar">
       <nav className="container">
         <NavLink to="/" className="brand">
           Eigen News
         </NavLink>
-        <Menu
-          mode="horizontal"
-          className="menu"
-          selectedKeys={[pathname]}
-          items={pageRoutes.map((route) => (
-            {
-              key: route.path,
-              label: <NavLink to={route.path}>{route.name}</NavLink>
-            }
-          ))}
-        />
+
+        {/* Desktop Mode */}
+        <div className="desktop-menu">
+          <Menu
+            mode="horizontal"
+            selectedKeys={[pathname]}
+            items={pageRoutes.map((route) => (
+              {
+                key: route.path,
+                // label: <NavLink to={route.path}>{route.name}</NavLink>
+                label: route.name
+              }
+            ))}
+          />
+        </div>
+
+        {/* Mobile Mode */} 
+        <Button 
+          type="text"
+          onClick={() => setOpen(true)}
+          className="mobile-menu-button"
+        >
+          <MenuOutlined />
+        </Button>
+        <Drawer 
+          title="Eigen News"
+          placement="right"
+          open={open}
+          onClose={() => setOpen(false)}
+          className="mobile-menu"
+        >
+          <Menu
+            mode="vertical"
+            selectedKeys={[pathname]}
+            items={pageRoutes.map((route) => (
+              {
+                key: route.path,
+                label: <NavLink to={route.path}>{route.name}</NavLink>
+              }
+            ))}
+          />
+        </Drawer>
       </nav>
     </Layout.Header>
   )
