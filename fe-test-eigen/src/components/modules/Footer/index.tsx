8ed6b868ce51afa5f9616e64dbb4527a5ffb2cdc
najimRizky <najimajim1@gmail.com>
2023-09-26 12:13:13 +0700

    feat(footer): adjust stye footer

--- fe-test-eigen/src/components/modules/Footer/index.tsx
+++ fe-test-eigen/src/components/modules/Footer/index.tsx
@@ -1,15 +1,15 @@
-import { Layout, Typography } from "antd"
+import { Layout } from "antd"
 import "./style.css"
 
 const Footer = () => {
   return (
-    <Layout className="footer">
-      <Layout.Footer>
-        <Typography>
+    <Layout.Footer className="footer">
+      <div className="container">
+        <p>
           Developped by <a href="https://najim-rizky.com" target="_blank">Najim Rizky</a>
-        </Typography>
-      </Layout.Footer>
-    </Layout>
+        </p>
+      </div>
+    </Layout.Footer>
   )
 }
 
