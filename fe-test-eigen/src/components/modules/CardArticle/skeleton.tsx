90fb728ff11dc25670d1f6c5b6ac392c00d8a1c3
najimRizky <najimajim1@gmail.com>
2023-09-27 08:29:56 +0700

    feat(card-article-skeleton): add id for testing

--- fe-test-eigen/src/components/modules/CardArticle/skeleton.tsx
+++ fe-test-eigen/src/components/modules/CardArticle/skeleton.tsx
@@ -2,7 +2,7 @@ import { Card, Skeleton } from 'antd'
 
 const CardArticleSkeleton = () => {
   return (
-    <Card>
+    <Card id='card-article-skeleton'>
       <Skeleton 
         active 
         paragraph={{ rows: 1 }} 
