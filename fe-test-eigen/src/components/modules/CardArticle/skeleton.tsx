7e31bb08389e5998c6ee8c8d6260a77df010a7d8
najimRizky <najimajim1@gmail.com>
2023-09-27 13:16:34 +0700

    refactor(card-article-skeleton): change id to data-testid

--- fe-test-eigen/src/components/modules/CardArticle/skeleton.tsx
+++ fe-test-eigen/src/components/modules/CardArticle/skeleton.tsx
@@ -2,7 +2,7 @@ import { Card, Skeleton } from 'antd'
 
 const CardArticleSkeleton = () => {
   return (
-    <Card id='card-article-skeleton'>
+    <Card data-testid='card-article-skeleton'>
       <Skeleton 
         active 
         paragraph={{ rows: 1 }} 
